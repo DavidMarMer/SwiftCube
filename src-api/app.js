c5b6ee618b9056bae4511e8425115bb1617623a0
DavidMarMer <alum.dmartinezm.2022@iesalixar.org>
2023-10-19 13:25:15 +0200

    cors fix

--- src-api/app.js
+++ src-api/app.js
@@ -29,12 +29,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  // origin: ['http://localhost:4200', 'https://davidmartinezmerencioalixar.github.io'],
-  origin: '*',
-  allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
-}));
+// app.use(cors({
+//   // origin: ['http://localhost:4200', 'https://davidmartinezmerencioalixar.github.io'],
+//   origin: '*',
+//   allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE']
+// }));
 app.use((req, res, next) => {
   for (const field in req.body) {
     const fieldValue = req.body[field];
@@ -45,6 +45,20 @@ app.use((req, res, next) => {
       req.body[field] = decryptedValue;
     }
   }
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
   next();
 });
 
