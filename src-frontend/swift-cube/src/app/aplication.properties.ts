e8ae8a8f68a2a7695385fcc48627b74866cf2ef6
DavidMarMer <davidmarmer02@gmail.com>
2025-03-25 17:06:20 +0100

    SSL

--- src-frontend/swift-cube/src/app/aplication.properties.ts
+++ src-frontend/swift-cube/src/app/aplication.properties.ts
@@ -1,4 +1,4 @@
 export const properties = {
     // apiUrl: 'http://localhost:5000'
-    apiUrl: 'http://conservation-george-swift-cube-78a1e113.koyeb.app'
+    apiUrl: 'https://conservation-george-swift-cube-78a1e113.koyeb.app'
 }
